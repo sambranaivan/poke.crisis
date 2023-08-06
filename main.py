fc01d5a9f2d236f24a1591c4812e763b2da56fae
sambrana <sambranaivan@gmail.com>
2023-08-06 14:17:29 -0300

    data para gastly

--- main.py
+++ main.py
@@ -4,27 +4,33 @@ from stats import make_stats
 from powers import make_sp
 import json
 from html import escape
+import pyperclip
 
 template = open('template.html')
 template = template.read()
-f = open('data.json')
+f = open('cards/Gastly.json')
 data = json.load(f)
 
 
 
 
+######BLOQUE DE ATAQUES
 
 attack_html  = '''<div class="mt-8 ml-7 px-2 py-1 w-213 h-[46rem] flex flex-col z-20">'''
 
 for attack in data["Attacks"]:
     attack_html += make_attack(attack)+'''<div class="flex flex-shrink h-[20px]"></div>'''
-# attack_html += '''</div>'''
+
 
 template = template.replace("@HealtyAttacks",attack_html)
 
-
+######BLOQUE DE STATS
 template = template.replace("@yield_stats",make_stats(data))
+
+######BLOQUE DE STATS HERIDO
 template = template.replace("@yield_inyured",make_stats(data))
+
+######BLOQUE DE HABILIDARES
 
 superpower_html = ""
 for super in data['Superpowers']:
@@ -33,12 +39,49 @@ superpower_html += '''</div>'''
 
 template = template.replace("@yield_superpowers",superpower_html)
 
+
+####### PORTRAIT
 port = (data["Name"]).replace(" ","%20")
-template = template.replace("#potrait#",portrait_url+port+".png")
+template = template.replace("#potrait#",portrait_url+data["Name"]+".png")
+
+################INJURED SIDE
+######BLOQUE DE ATAQUES
+
+attack_html  = '''<div class="mt-8 ml-7 px-2 py-1 w-213 h-[46rem] flex flex-col z-20">'''
+
+for attack in data["Attacks"]:
+    attack_html += make_attack(attack)+'''<div class="flex flex-shrink h-[20px]"></div>'''
+##suma el nuevo ataque
+for attack in data["Injured Side"]["Attacks"]:
+    attack_html += make_attack(attack)+'''<div class="flex flex-shrink h-[20px]"></div>'''
+
+template = template.replace("@injured_attacks",attack_html)
+
+######BLOQUE DE HABILIDARES
+
+superpower_html = ""
+for super in data['Superpowers']:
+    superpower_html += make_sp(super)+'''<div class="flex flex-shrink h-[20px]"></div>'''
+
+for super in data["Injured Side"]['Superpowers']:
+    superpower_html += make_sp(super)+'''<div class="flex flex-shrink h-[20px]"></div>'''
+
+superpower_html += '''</div>'''
+
+template = template.replace("@injured_sp",superpower_html)
+
+
+#FIN
 
 h = open("results.html", "w")
-
+pyperclip.copy(template)
 h.write(template)
 h.close()
 
 # print(template)
+"""
+var script = document.createElement('script');
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(script);
+$(".stat-card-inner").html("")
+"""
--- stats.py
+++ stats.py
@@ -1,22 +1,28 @@
+from base import icon_url
+import json
+
+
+f = open('icons.json')
+icon = json.load(f)
 def make_stats(data):
     html_template = '''<div class="mt-8 ml-6 w-67 h-[220px] z-20">
                 <div class="font-face-agency-fb-black-condensed text-center" style="font-size: 35px; line-height: 35px; font-weight: bold; letter-spacing: 3px;">#NAME#</div>
-                <div class="font-face-noto-sans-condensed text-center tracking-widest flex flex-wrap justify-center" style="font-size: 20px; line-height: 22px;"><span class="whitespace-pre"><span class="leading-4 tracking-normal mr-px text-xl">A</span><span class="tracking-wider leading-3 text-base"></span></span></div>
+                <div class="font-face-noto-sans-condensed text-center tracking-widest flex flex-wrap justify-center" style="font-size: 20px; line-height: 22px;"><span class="whitespace-pre"><span class="leading-4 tracking-normal mr-px text-xl">#ALTER#</span><span class="tracking-wider leading-3 text-base"></span></span></div>
                 <div class="flex flex-row justify-between mt-4">
                     <div class="pill-with-badge relative flex flex-row h-11"><span class="bg-black rounded-full z-10">
-                            <div class="h-full text-white flex flex-col justify-center align-center w-11 text-2xl"><img src="https://www.jarvis-protocol.com/assets/images/e9d295985efdd834e3c7.png" class="rounded-full w-11 border-solid border-2 border-black"></div>
+                            <div class="h-full text-white flex flex-col justify-center align-center w-11 text-2xl"><img src="#wtype#" class="rounded-full w-11 border-solid border-2 border-black"></div>
                         </span>
                         <div class="flex flex-col align-center -ml-3 self-center"><span class="pill bg-black py-1 text-white font-bold font-face-noto-sans-condensed-bold whitespace-nowrap rounded-r-full w-13 text-center pl-4 pr-4 leading-5 text-2xl"><span class="whitespace-pre"><span class="leading-5 text-2xl">#WEAK#</span></span></span></div>
                     </div>
                     <div class="pill-with-badge relative flex flex-row h-11"><span class="bg-black rounded-full z-10">
-                            <div class="h-full text-white flex flex-col justify-center align-center w-11 text-2xl"><img src="https://www.jarvis-protocol.com/assets/images/0107812b0ce2a706b9eb.png" class="rounded-full w-11 border-solid border-2 border-black"></div>
-                        </span>
-                        <div class="flex flex-col align-center -ml-3 self-center"><span class="pill bg-black py-1 text-white font-bold font-face-noto-sans-condensed-bold whitespace-nowrap rounded-r-full w-13 text-center pl-4 pr-4 leading-5 text-2xl"><span class="whitespace-pre"><span class="leading-5 text-2xl">#NORMAL#</span></span></span></div>
-                    </div>
-                    <div class="pill-with-badge relative flex flex-row h-11"><span class="bg-black rounded-full z-10">
-                            <div class="h-full text-white flex flex-col justify-center align-center w-11 text-2xl"><img src="https://www.jarvis-protocol.com/assets/images/74cda4cceec102296947.png" class="rounded-full w-11 border-solid border-2 border-black"></div>
+                            <div class="h-full text-white flex flex-col justify-center align-center w-11 text-2xl"><img src="#rtype#" class="rounded-full w-11 border-solid border-2 border-black"></div>
                         </span>
                         <div class="flex flex-col align-center -ml-3 self-center"><span class="pill bg-black py-1 text-white font-bold font-face-noto-sans-condensed-bold whitespace-nowrap rounded-r-full w-13 text-center pl-4 pr-4 leading-5 text-2xl"><span class="whitespace-pre"><span class="leading-5 text-2xl">#RESISTANCE#</span></span></span></div>
+                    </div>
+                    <div class="pill-with-badge relative flex flex-row h-11"><span class="bg-black rounded-full z-10">
+                            <div class="h-full text-white flex flex-col justify-center align-center w-11 text-2xl"><img src="#dtype#" class="rounded-full w-11 border-solid border-2 border-black"></div>
+                        </span>
+                        <div class="flex flex-col align-center -ml-3 self-center"><span class="pill bg-black py-1 text-white font-bold font-face-noto-sans-condensed-bold whitespace-nowrap rounded-r-full w-13 text-center pl-4 pr-4 leading-5 text-2xl"><span class="whitespace-pre"><span class="leading-5 text-2xl">#NORMAL#</span></span></span></div>
                     </div>
                 </div>
                 <div class="flex flex-row mx-6 justify-around my-1">
@@ -46,13 +52,17 @@ def make_stats(data):
             </div>'''
     # Replace the placeholders with the corresponding values from the JSON
     replaced_html = html_template.replace("#NAME#",str(data["Name"]))
+    replaced_html = replaced_html.replace("#ALTER#",str(data["Alter Ego"]))
     replaced_html = replaced_html.replace("#WEAK#",str(data["Weak Defense"]))
     replaced_html = replaced_html.replace("#NORMAL#",str(data["Defense"]))
-    replaced_html = replaced_html.replace("#RESISTANCE#",str(data["Resistanse Defense"]))
+    replaced_html = replaced_html.replace("#RESISTANCE#",str(data["Resistant Defense"]))
     replaced_html = replaced_html.replace("#STAMINA#",str(data["Stamina"]))
     replaced_html = replaced_html.replace("#THREAT#",str(data["Threat Level"]))
     replaced_html = replaced_html.replace("#SIZE#",str(data["Size"]))
     replaced_html = replaced_html.replace("#MOVE#",str(data["Movement"]))
+    replaced_html = replaced_html.replace("#rtype#",icon_url+icon[data["Resistant Type"]])
+    replaced_html = replaced_html.replace("#wtype#",icon_url+icon[data["Weak Type"]])
+    replaced_html = replaced_html.replace("#dtype#",icon_url+icon["normal"])
 
     # print(data["NAME"][0])
     return replaced_html
