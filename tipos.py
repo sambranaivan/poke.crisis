8c3c53df86502c1f81b9804b8550170e30246f17
sambrana <sambranaivan@gmail.com>
2023-08-06 12:01:43 -0300

    iconos de poderes

--- tipos.py
+++ tipos.py
@@ -1,6 +1,31 @@
 
-base_url = 
+
 
 tipos = {
-    steel = 
+'Bug':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Bug.png',
+'Dark':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Dark.png',
+'Dragon':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Dragon.png',
+'Electric':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Electric.png',
+'Fairy':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Fairy.png',
+'Fighting':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Fighting.png',
+'Fire':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Fire.png',
+'Flying':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Flying.png',
+'Ghost':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Ghost.png',
+'Grass':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Grass.png',
+'Ground':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Ground.png',
+'Ice':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Ice.png',
+'Normal':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Normal.png',
+'Poison':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Poison.png',
+'Psychic':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Psychic.png',
+'Rock':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Rock.png',
+'Steel':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Steel.png',
+'Water':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/Pokemon_Type_Icon_Water.png'
 }
+
+superpowers = {
+    'reactive':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/reactive.png',
+    'innate':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/innate.png',
+    'leader':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/leader.png',
+    'active':'https://github.com/sambranaivan/poke.crisis/blob/cfb4fc6e636210494144073f901415decbf30bf2/icons/active.png'
+}
+
