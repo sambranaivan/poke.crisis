bb230643cf47375a847526a21d32959b30c48970
sambrana <sambranaivan@gmail.com>
2023-12-18 11:16:59 -0300

    Squiortle stat cards

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from powers import make_sp
 import json
 from html import escape
 import pyperclip
-mcp = 1
-chara = 'Gifteon'
+mcp = 0
+chara = 'Squirtle'
 template = open('template.html')
 
 if mcp:
