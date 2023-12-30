007ed232d47bd023c1db4c71a4e0d2acc432d32b
sambrana <sambranaivan@gmail.com>
2023-12-30 10:43:00 -0300

    rename

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from powers import make_sp
 import json
 from html import escape
 import pyperclip
-mcp = 1
-chara = 'mega alakazam'
+mcp = 0
+chara = 'greninjash'
 template = open('template.html')
 
 if mcp:
