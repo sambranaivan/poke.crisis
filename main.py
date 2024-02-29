955b606b8f2be129eb88c0650ee6b007e90bc95d
sambrana <sambranaivan@gmail.com>
2024-02-29 20:40:33 -0300

    reanme paradox beast

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from powers import make_sp
 import json
 from html import escape
 import pyperclip
-mcp = 0
-chara = 'walking wake'
+mcp = 1
+chara = 'paradox beast'
 template = open('template.html')
 
 if mcp:
