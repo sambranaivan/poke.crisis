3ddf974f37e0bd57e478155525d36dc3185c846a
sambrana <sambranaivan@gmail.com>
2024-10-16 21:13:01 -0300

    fearrow pacj

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from powers import make_sp
 import json
 from html import escape
 import pyperclip
-mcp = 1
-chara = 'Galarian Zapdos'
+mcp = 0
+chara = 'Mega Charizard Y'
 template = open('template.html')
 
 if mcp:
