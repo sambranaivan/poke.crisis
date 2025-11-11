0902a0dc129640d76115fa12fa1c09a5d12868bc
Ivan Sambrana <sambranaivan@gmail.com>
2025-11-11 17:24:04 -0300

    igglypuff

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from powers import make_sp
 import json
 from html import escape
 import pyperclip
-mcp = 1
-chara = 'Jirachi'
+mcp = 0
+chara = 'Wigglytuff'
 template = open('template.html')
 
 if mcp:
